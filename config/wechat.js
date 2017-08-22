4eb73cb119c55ac94ebbca9e911f8e67ab67167e
lwei <1390996503@qq.com>
2017-08-22 22:53:34 +0800

    proxy

---
+++ config/wechat.js
@@ -0,0 +1,4 @@
+module.exports = {
+    appid: "wx0d215ec078c80bc0",
+    secret: "a6ee3f5b61b3ee85307840f5853d2785",
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var wx = require('../config/wechat');
 
+var OAuth = require('wechat-oauth');
+var api = new OAuth(wx['appid'], wx['secret']);
+
+var url = api.getAuthorizeURL('http://101.132.38.190/wechat', 'state', 'snsapi_userinfo');
+var options = {
+  maxAge: 1000 * 60 * 30,
+  httpOnly: false
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/wechat', function (req, res, next) {
+  api.getAccessToken(req.query.code, function (err, result) {
+    if (err != null) {
+      res.redirect('/wechat');
+    } else {
+
+      var ak = result.data.access_token;
+      var openid = result.data.openid;
+      res.cookie('kt_openid', openid, options)
+
+      api.getUser(openid, function (err, result) {
+        res.render('index', {
+          title: 'KT足球'
+        });
+        // axios.post(config["bucky"]["host"] + '/api/v1/users/signup/oauth?app_key=' + config["bucky"]["app_key"],
+        //   querystring.stringify({
+        //     openid: result.openid,
+        //     source: 'wechat',
+        //     unionid: result.unionid,
+        //     source_name: result.nickname,
+        //     source_avatar: result.headimgurl
+        //   }))
+        //   .then(function (response) {
+        //     res.cookie('kt_wx_info', JSON.stringify(result), options)
+        //     res.cookie('kt_user_info', JSON.stringify(response.data), options)
+
+        //     res.render('index.html', {
+        //       title: 'KT足球'
+        //     });
+        //   })
+        //   .catch(function (error) {
+        //     console.log(error);
+        //   });
+
+      });
+    }
+  })
 });
 
 module.exports = router;
