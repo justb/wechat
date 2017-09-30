72c3859433f5cb723c14218a96931edbdc1e2145
weiyuan <1390996503@qq.com>
2017-09-30 23:51:57 +0800

    wechat

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  var OAuth = require('wechat-oauth');
+  var client = new OAuth('wx0d215ec078c80bc0', 'a6ee3f5b61b3ee85307840f5853d2785');
+  var url = client.getAuthorizeURL('redirectUrl', 'state', 'scope');
+  client.getAccessToken('code', function (err, result) {
+    var accessToken = result.data.access_token;
+    var openid = result.data.openid;
+  });
+  client.getUser(openid, function (err, result) {
+    var userInfo = result;
+    console.log(result)
+  });
 });
 
 module.exports = router;
